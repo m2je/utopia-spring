0b73a5d00b137c10e511d517506926eaaf4b1c92
mehdi <mehdis@hyperoffice.com>
2014-01-02 06:42:28 -0500

    init

--- utopia-ui/test/java/com/utopia/core/user/UserCollectionResult.java
+++
@@ -1,8 +0,0 @@
-package com.utopia.core.user;
-
-import com.utopia.core.model.CollectionResult;
-import com.utopia.core.security.model.CoUser;
-
-public class UserCollectionResult extends CollectionResult<CoUser>{
-
-}
